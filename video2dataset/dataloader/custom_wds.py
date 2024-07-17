9d36c74e1350c1aceaa743c1a1d1ae0b620f8020
Miquel Farre <miquel.farre@huggingface.co>
2024-07-17 16:36:14 +0200

    do not crash when torchdistributed is not being used

--- video2dataset/dataloader/custom_wds.py
+++ video2dataset/dataloader/custom_wds.py
@@ -507,7 +507,7 @@ class TorchDataWebdataset(DataPipeline, FluidInterfaceWithChangedDecode):
             main_datapipe.apply_sharding(world_size, global_rank)
             # synchronize data across processes to prevent hanging if sharding is uneven (which is likely)
             main_datapipe = main_datapipe.fullsync()
-        except RuntimeError:
+        except Exception as e:
             print("torch distributed not used, not applying sharding in dataloader")
             pass
         # start shuffling accross shards for the first time to mix different datasets
